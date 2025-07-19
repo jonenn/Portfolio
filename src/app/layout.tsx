699865a8e505082b84cc6a75479be086d41d89fa
Jonenn <juanmagri@outlook.es>
2025-07-19 02:15:58 -0500

    Temporary homepage content added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Footer } from "@/components/layout/Footer";
 const montserrat = Montserrat({
    variable: "--font-montserrat",
    subsets: ["latin"],
-   weight: ["400", "700"],
+   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
--- src/components/common/Hero/index.tsx
+++ src/components/common/Hero/index.tsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
 import "./Hero.scss";
 
 export const Hero = () => {
    return (
       <main>
-         <div className="container">Hello!</div>
+         <div className="hero__container container">
+            <div className="hero__container--left">
+               <Image
+                  src="https://res.cloudinary.com/dedihqcsy/image/upload/v1752903606/hero.16da25d7_2_xkyuli.png"
+                  alt="Hero image"
+                  width={503}
+                  height={419}
+               />
+            </div>
+            <div className="hero__container--right">
+               <h1 className="hero__title">
+                  I’m Jonenn, a problem-solving developer with a designer’s eye
+               </h1>
+               <p className="hero__content">
+                  I am a full-stack JavaScript developer and build user-centered
+                  web apps that balance performance, usability, and visual
+                  polish with the advantages of both worlds.
+               </p>
+            </div>
+         </div>
       </main>
    );
 };
