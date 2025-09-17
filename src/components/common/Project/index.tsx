5c7c670f71cb64fca7ec338366320e0ac155b601
Juan J Malaver <juanmagri@outlook.es>
2025-09-17 17:22:56 -0500

    Additional styles for project component

--- src/components/common/Project/index.tsx
+++ src/components/common/Project/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import './Project.scss';
 import background from '../../../assets/ProjectBG.svg';
-import Link from 'next/link';
 
 type ProjectTypes = {
    id: number;
@@ -48,20 +47,37 @@ const Project = ({ project }: PortfolioTypes) => {
                </div>
             </div>
             <div className="project__description">
-               <h3 className="project__year">{project.year}</h3>
-               <h4 className="project__title">{project.title}</h4>
-               <p className="project__content">{project.description}</p>
-               <div className="project__buttons">
-                  <button className="project__button"></button>
-                  <button className="project__button"></button>
+               <h3 className="description__item description__item--year">
+                  {project.year}
+               </h3>
+               <h4 className="description__item description__item--title">
+                  {project.title}
+               </h4>
+               <p className="description__item description__item--content">
+                  {project.description}
+               </p>
+               <div className="description__item--buttons">
+                  <button className="description__item description__item--button">
+                     View Design
+                  </button>
+                  <button className="description__item description__item--button">
+                     View Deployment
+                  </button>
                </div>
-               <div className="project__techstack">
-                  <div className="project__tech"></div>
+               <div className="stack__list">
+                  <h4 className="description__item--title">Tech Stack</h4>
+                  <div className="description__item description__item--tech">
+                     React
+                  </div>
                </div>
-               <div className="project__labels">
-                  <div className="project__caption">Labels:</div>
-                  <div className="project__label">{project.label1}</div>
-                  <div className="project__label">{project.label2}</div>
+               <div className="description__item--labels">
+                  <div className="description__item--caption">Labels:</div>
+                  <div className="description__item description__item--label">
+                     {project.label1}
+                  </div>
+                  <div className="description__item description__item--label">
+                     {project.label2}
+                  </div>
                </div>
             </div>
          </div>
